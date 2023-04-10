893ed4aeb5123788c3174aa25a24e1b9ad883e56
Bohemian36 <127652859+Bohemian36@users.noreply.github.com>
2023-04-10 05:24:12 -0300

    Update ItemCarrito.java

--- ItemCarrito.java
+++ ItemCarrito.java
@@ -8,16 +8,16 @@ package pack1;
         private double subTotal;
 	
     	public ItemCarrito(Carrito num, Producto p, int cantidad) {
-		       ticket = num;
-		       prod = p;
-	    	   this.cantidad=cantidad;
+		ticket = num;
+		prod = p;
+	    	this.cantidad=cantidad;
     	   	subTotal = prod.un_precio() * cantidad;	
-	}
+        }
     	public double SubTotal() {
     	    return subTotal;
-	}
+        }
     	public void mostrarItem() {
-	       	 System.out.println("Producto: "+ prod.un_nombre());
+	    System.out.println("Producto: "+ prod.un_nombre());
     	    System.out.println("Cantidad: " +cantidad);
     	    System.out.println("Precio: "+prod.un_precio());
     	    System.out.println("Sub Total: "+ subTotal);
